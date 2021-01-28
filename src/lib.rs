6628506827ab5391c36578d9d4eec05285ed6979
Danilo Guanabara <danguafer@gmail.com>
2021-01-27 23:02:26 -0300

    Added cross-test as a dep

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
+    use cross_test::prelude::*;
+
+    cross_test_configure!();
+
+    #[cross_test]
+    async fn it_works() {
         assert_eq!(2 + 2, 4);
     }
 }
